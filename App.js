78a66a35bb5b451bcb462f0845fd1cd9394e3f38
Swetha246 <136987864+Swetha246@users.noreply.github.com>
2023-06-24 21:33:52 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,1 +1,13 @@
+import React from "react";
+import Sample from './assign1/Sample';
 
+function App() {
+  return (
+    <div className ="App">
+     <Sample/> 
+
+    </div>
+  );
+}
+
+export default App;
